96e04bf512f3f48c0520aed87a334e478c4e0523
ArseniSkobelev <arseni.skobelev0@gmail.com>
2023-05-15 08:46:32 +0000

    k8s_configure_node.sh v1.0 release

--- kubernetes/k8s_configure_node.sh
+++ kubernetes/k8s_configure_node.sh
@@ -9,27 +9,19 @@
 # Exit script on error
 set -e
 
+define_colors() {
 # Define text-log colors
 ERROR='\033[0;31m'
 INFO='\033[0;33m'
 SUCCESS='\033[0;32m'
 NC='\033[0m'
+}
 
-# ----------------------
-# |    Disable swap    |
-# ----------------------
-echo $INFO"[Step 1 | Swap] Disabling swap"
-
-sudo swapoff -a >> /dev/null
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab >> /dev/null
-
-echo $SUCCESS"[Step 1 | Swap] Swap disabled successfully!"$NC
-
-
+kernel_settings() {
 # --------------------------------------------
 # |    Add kernel settings and set params    |
 # --------------------------------------------
-echo $INFO"[Step 2 | Kernel] Adding kernel settings"$NC
+echo $INFO"[Step 2/5 | Kernel] Adding kernel settings"$NC
 
 sudo tee /etc/modules-load.d/containerd.conf >> /dev/null <<EOF
 overlay
@@ -44,20 +36,19 @@ net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
-sudo sysctl --ignore --quiet --system >> /dev/null
+sudo sysctl --quiet --system --ignore 2>/dev/null
 
-echo $SUCCESS"[Step 2 | Kernel] Kernel settings added successfully!"$NC
+echo $SUCCESS"[Step 2/5 | Kernel] Kernel settings added successfully!"$NC
+}
 
-
+install_containerd() {
 # ----------------------------
 # |    Install Containerd    |
 # ----------------------------
-echo $INFO"[Step 3 | Container runtime] Installing Containerd.."
+echo $INFO"[Step 3/5 | Container runtime] Installing Containerd.."
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg >> /dev/null
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg >> /dev/null
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" --yes >> /dev/null
-
-asdasdasdasd
 
 sudo apt-get update >> /dev/null
 sudo apt-get install -y containerd.io >> /dev/null
@@ -67,28 +58,51 @@ sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/c
 sudo systemctl restart containerd >> /dev/null
 sudo systemctl enable containerd >> /dev/null
 
-echo $SUCCESS"[Step 3 | Container runtime] Containerd installed successfully!"$NC
+echo $SUCCESS"[Step 3/5 | Container runtime] Containerd installed successfully!"$NC
+}
 
-
+install_k8s() {
 # ---------------------------
 # |    Install K8s tools    |
 # ---------------------------
-echo $INFO"[Step 4 | Kubernetes] Installing K8s tooling"
+echo $INFO"[Step 4/5 | Kubernetes] Installing K8s tooling"
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg >> /dev/null
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --batch --yes --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg >> /dev/null
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" --yes >> /dev/null
 
 sudo apt-get update >> /dev/null
 sudo apt-get install -y kubelet kubeadm kubectl >> /dev/null
 sudo apt-mark hold kubelet kubeadm kubectl >> /dev/null
 
-echo $SUCCESS"[Step 4 | Kubernetes] All of the required K8s tools installed successfully"$NC
+echo $SUCCESS"[Step 4/5 | Kubernetes] All of the required K8s tools installed successfully"$NC
+}
 
-
+display_success_message() {
 # -------------------
 # |    Summarize    |
 # -------------------
 
 host=$(hostname)
 
-echo -e "\n"$SUCCESS$host "| Node ready for action!"$NC
+echo $SUCCESS"[Step 5/5 | $host] Ready for action"$NC
+}
+
+disable_swap() {
+# ----------------------
+# |    Disable swap    |
+# ----------------------
+echo $INFO"[Step 1/5 | Swap] Disabling swap"
+
+sudo swapoff -a >> /dev/null
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab >> /dev/null
+
+echo $SUCCESS"[Step 1/5 | Swap] Swap disabled successfully!"$NC
+}
+
+# run
+define_colors
+disable_swap
+kernel_settings
+install_containerd
+install_k8s
+display_success_message
